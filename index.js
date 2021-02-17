bb3796af1b89aebb692f18575c1fcec4d81f80ec
Alireza <alirezachaviwala@gmail.com>
2021-02-17 07:10:32 +0530

    front end url update2

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const dbName = 'FirstOrgCluster';
 
 app.use(bodyParser.json());
 app.use(cors({
-    origin: 'https://happy-jones-a6ae4e.netlify.app/',
+    origin: 'https://happy-jones-a6ae4e.netlify.app',
     optionsSuccessStatus: 200 // For legacy browser support
 }));
 app.listen(process.env.PORT || 8080);
