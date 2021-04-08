923e50a3dc61498585eaf972af6fd282031bcd4d
Alireza <alirezachaviwala@gmail.com>
2021-04-08 22:41:25 +0530

    jwt helper updated

--- helpers/jwt_helper.js
+++ helpers/jwt_helper.js
@@ -20,8 +20,8 @@ module.exports = {
     authenticateAccessToken: (req, res, next) => {
         if (!req.headers.authorization) return next(createError.Unauthorized())
 
-        const header = req.headers.authorization.split(" ");
-        const token = header[0];
+        let header = req.headers.authorization.split(" ");
+        let token = header[1];
         jwt.verify(token, process.env.ACCESS_TOKEN_KEY, (err, payload) => {
             if (err) {
                 const message = err.name === 'JsonWebTokenError' ? 'Unauthorized' : err.message;
